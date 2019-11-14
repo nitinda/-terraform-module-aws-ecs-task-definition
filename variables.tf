b16a94f67e21a96e060c892ad7421d092caca43b
nitin-das <nitin.das@thomson.co.uk>
2019-11-14 09:58:36 +0000

    Modified code

---
+++ variables.tf
@@ -0,0 +1,49 @@
+## ECS Task Definition
+variable "family" {
+  description = "A unique name for your task definition."
+}
+
+variable "container_definitions" {
+  description = "A list of valid container definitions provided as a single valid JSON document."
+}
+
+variable "task_role_arn" {
+  description = "The ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services."
+}
+
+variable "execution_role_arn" {
+  description = "The Amazon Resource Name (ARN) of the task execution role that the Amazon ECS container agent and the Docker daemon can assume."
+}
+
+variable "network_mode" {
+  description = "The Docker networking mode to use for the containers in the task."
+}
+
+variable "placement_constraints" {
+  description = "A set of placement constraints rules that are taken into consideration during task placement."
+  type        = "list"
+}
+
+variable "cpu" {
+  description = "The number of cpu units used by the task."
+}
+
+variable "memory" {
+  description = "The amount (in MiB) of memory used by the task. If the requires_compatibilities is FARGATE this field is required."
+}
+
+variable "requires_compatibilities" {
+  description = "A set of launch types required by the task."
+  type        = "list"
+}
+
+variable "volume" {
+  description = "A set of volume blocks that containers in your task may use."
+  type        = "list"
+}
+
+## Tags
+variable common_tags {
+  description = "Resources Tags"
+  type        = "map"
+}
